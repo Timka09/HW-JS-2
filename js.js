273371544f4f80f0e48452abad3bf5f32dd31e98
Timka09 <tymofij.i.naumenko@gmail.com>
2024-09-24 21:08:23 +0300

    8 tasks

---
+++ js.js
@@ -0,0 +1,38 @@
+// Task 1
+const celsius = 25;
+const ferenheit = (celsius * 9) / 5 + 32;
+console.log(ferenheit + "◦F");
+// Task 2
+const month = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30);
+const caclulation = (month * 24);
+console.log(caclulation);
+const minutes = (caclulation * 60);
+console.log(minutes);
+// Task 3
+const health = 100;
+const energy = 150;
+const minusHealth = (health - 20);
+const minusEnergy = (energy - 50);
+console.log(minusHealth);
+console.log(minusEnergy);
+// Task 4
+const price = 100;
+const sale = (price - 10);
+console.log(sale + "грн");
+// Task 5
+const number = 14.678234;
+const rounding = Math.floor(number);
+console.log(rounding);
+// Task 6
+const decimals = "67.34586345";
+const numbering = Number.parseFloat(decimals);
+console.log(numbering);
+// Task 7
+const wholeNumber = "67";
+const numbers = Number.parseInt(wholeNumber);
+console.log(wholeNumber);
+// Task 8
+const squareRoot = 49
+const root = Math.sqrt(squareRoot)
+console.log(root)
+// Task 9 
